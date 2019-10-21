dbdf215394b6d912b5859ae6be5920c3afc538ec
Irtiola <irtiola@gmail.com>
2019-10-21 09:52:24 -0700

    changed css path

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+const path = require("path");
+app.use(express.static(__dirname + '/public'));
 
 var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
@@ -10,13 +12,13 @@ const cheerio = require('cheerio')
 
 var request = require('request');
 
-app.get("/index", function (req, res) {
+app.get("/", function(req, res) {
     res.render("index");
 });
 
 
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Listening on 3000");
 });
