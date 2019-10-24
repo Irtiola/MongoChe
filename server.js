ab237b566a057936b5b093b0d3338a92fbf448f1
Irina K <irtiola@gmail.com>
2019-10-23 22:08:39 -0700

    scraping to node || no front end yet

--- server.js
+++ server.js
@@ -1,22 +1,76 @@
-var express = require("express");
-var app = express();
+// var express = require("express");
+// var app = express();
 
-var bodyParser = require("body-parser");
-app.set("view engine", "ejs");
+// var bodyParser = require("body-parser");
+// app.set("view engine", "ejs");
 
-const mongojs = require('mongojs')
+// const mongojs = require('mongojs')
 
-const cheerio = require('cheerio')
+// const cheerio = require('cheerio')
 
-var request = require('request');
+// var request = require('request');
 
-app.get("/index", function (req, res) {
-    res.render("index");
-});
+// app.get("/index", function (req, res) {
+//     res.render("index");
+// });
+// app.get("/saved", function (req, res) {
+//     res.render("saved");
+// });
 
 
 
+// app.listen(process.env.PORT || 4000, function () {
+//     console.log("Listening on 4000");
+// });
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log("Listening on 3000");
-});
+var cheerio = require("cheerio");
+var Nightmare = require('nightmare');
+
+// First, tell the console what server.js is doing
+console.log("\n***********************************\n" +
+    "Grabbing every thread name and link\n" +
+    "from reddit's webdev board:" +
+    "\n***********************************\n");
+
+// Making a request via axios for reddit's "webdev" board. We are sure to use old.reddit due to changes in HTML structure for the new reddit. The page's Response is passed as our promise argument.
+var scrape = new Nightmare({
+    show: true,
+    waitTimeout: 1000 * 4
+})
+    .goto("https://www.theguardian.com/us/lifeandstyle")
+    .evaluate(function () {
+        return document.body.innerHTML;
+    }).end().then(function (html) {
+
+        // Load the Response into cheerio and save it to a variable
+        // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+        var $ = cheerio.load(html);
+
+        // An empty array to save the data that we'll scrape
+        var results = [];
+
+        // With cheerio, find each p-tag with the "title" class
+        // (i: iterator. element: the current element)
+        $("h3.fc-item__title").each(function (i, element) {
+
+            // Save the text of the element in a "title" variable
+            var title = $(element).text();
+
+            // In the currently selected element, look at its child elements (i.e., its a-tags),
+            // then save the values for any "href" attributes that the child elements may have
+            var link = $(element).children().eq(0).attr("href");
+
+            // if (!link.includes('https://old.reddit.com/r/webdev')) {
+            //     link = "https://old.reddit.com/r/webdev" + link;
+            // }
+
+            // Save these results in an object that we'll push into the results array we defined earlier
+            results.push({
+                title: title,
+                link: link
+            });
+        });
+
+        // Log the results once you've looped through each of the elements found with cheerio
+        console.log(results);
+    });
