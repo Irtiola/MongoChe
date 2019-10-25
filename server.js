602f8ec16ac439c80476760acbc1279c4863fbb1
Irtiola <irtiola@gmail.com>
2019-10-25 15:22:18 -0700

    articles renders on frontend

--- server.js
+++ server.js
@@ -1,39 +1,77 @@
-var express = require("express");
-var app = express();
-const path = require("path");
-app.use(express.static(__dirname + '/public'));
+const express = require("express");
+const app = express();
 
-var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 
-const mongojs = require('mongojs')
+const bodyParser = require("body-parser");
+app.use(bodyParser())
 
-const cheerio = require('cheerio')
+const cheerio = require("cheerio");
 
-var request = require('request');
+const mongojs = require("mongojs");
+
+const databaseUrl = "scraper";
+const collections = ["scrapedData"];
+
+const request = require("request");
+
+const db = mongojs(databaseUrl, collections);
+
+
+db.on("error", function(error) {
+    console.log("Database Error:", error);
+});
+
+function updateDb(title, link, content) {
+    db.scrapedData.update({ Title: title, Link: link }, { $set: { Title: title, Link: link } }, { upsert: true }, );
+};
+
+app.post("/saveArticle", function(req, res) {
+
+    var title = req.body.title;
+    var link = req.body.link;
+    var content = req.body.content;
+    updateDb(title, link, content);
+})
+
+app.get("/savedArticles", function(req, res) {
+    db.scrapedData.find(function(error, result) {
+        res.render("savedArticles", { results: result });
+    });
+});
+
+app.post("/addComment", function(req, res) {
+    var id = req.body.id;
+    var comment = req.body.newComment;
+    db.scrapedData.update({ "_id": mongojs.ObjectID(id) }, { $push: { Comments: comment } }, function(error, response) {
+        if (error) {
+            console.log(error)
+        }
+    });
+    res.redirect("/savedArticles");
+})
+
 
 app.get("/", function(req, res) {
-    res.render("index");
+    request.get("https://www.theguardian.com/us", function(err, response, body) {
+        var $ = cheerio.load(body);
+        var results = [];
+        $("h3.fc-item__title ").each(function(i, element) {
+            var title = $(element).text();
+            var link = $(element).children().eq(0).attr("href");
+            // var text = $(element).children().eq(1).attr(".fc-item__kicker")
+            if (link[0] == "/") {
+                link = "https://www.theguardian.com" + link;
+            }
+            if (title != "")
+                results.push({ Title: title, Link: link });
+        });
+
+        res.render("index", { results: results });
+    });
 });
 
-app.get("/index", function(req, res) {
-    res.render("index");
-});
 
-app.get("/articles", function(req, res) {
-    res.render("articles")
-});
-
-app.post("/save", function(req, res) {
-
-
-});
-app.post("/addComment", function(req, res) {
-    var userComment = req.body.userComment;
-
-});
-
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Listening on 3000");
+app.listen("3000", function() {
+    console.log("Server running on 3000");
 });
